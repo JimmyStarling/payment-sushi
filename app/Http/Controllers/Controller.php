28034299a4505e4b568220e9141573bb3f99b745
Jimmy Starling <jimmystarlingdev@gmail.com>
2025-08-06 23:15:29 -0300

    doc/feat: add swagger description to root controller.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,22 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *     title="API Sistema de pagamentos - Sushi Pay",
+ *     version="0.1.0"
+ * )
+ *
+ * @OA\SecurityScheme(
+ *     securityScheme="bearerAuth",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT",
+ *     in="header",
+ *     name="Authorization"
+ * )
+ */
 abstract class Controller
 {
-    //
+    
 }
