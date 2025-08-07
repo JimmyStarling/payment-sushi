1f56fca4e480ad02282093776e6ccc1d2ca72047
Jimmy Starling <jimmystarlingdev@gmail.com>
2025-08-06 23:17:10 -0300

    doc: add properties description to swagger and docs.

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,6 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+/**
+ * @property string $id
+ * @property string $user_id
+ * @property string $status
+ * @property float $total
+ * @property array $items
+ * @property array $tags
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ *
+ * @property-read User $user
+ */
 class Order extends Model
 {
     use HasFactory;
