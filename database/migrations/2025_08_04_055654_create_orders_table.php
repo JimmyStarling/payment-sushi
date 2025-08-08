fd9fc3e85a4f7da70fb66f17c405d995b0c2008d
Jimmy Starling <jimmystarlingdev@gmail.com>
2025-08-08 01:03:16 -0300

    refactor: change primary key id to a uuid.

--- database/migrations/2025_08_04_055654_create_orders_table.php
+++ database/migrations/2025_08_04_055654_create_orders_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('orders', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();//->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade'); // cliente
             $table->string('status')->default('pending');
             $table->json('items');
