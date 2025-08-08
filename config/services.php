5e62c4d0ef260b0e7cce0193219680ff44ee3622
Jimmy Starling <jimmystarlingdev@gmail.com>
2025-08-08 01:02:39 -0300

    core: define twilio as services.

--- config/services.php
+++ config/services.php
@@ -34,5 +34,11 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
+    'twilio' => [
+        'sid' => env('TWILIO_SID'),
+        'token' => env('TWILIO_TOKEN'),
+        'whatsapp_number' => env('TWILIO_WHATSAPP_NUMBER'),
+        'whatsapp_recipient_number' => env('TWILIO_WHATSAPP_RECIPIENT_NUMBER'),
+    ],
 
 ];
